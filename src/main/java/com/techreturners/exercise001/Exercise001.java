04b037080e9d0910b02964034f1d19426012d092
Your Correct Name <andystyles30@gmail.com>
2020-08-27 13:35:44 +0100

    Implement exercise001 generateInitials

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -12,7 +12,7 @@ public class Exercise001 {
     }
 
     public String generateInitials(String firstName, String lastName) {
-        return "";
+        return firstName.substring(0, 1) + '.' + lastName.substring(0, 1);
     }
 
     public double addVat(double originalPrice, double vatRate) {
