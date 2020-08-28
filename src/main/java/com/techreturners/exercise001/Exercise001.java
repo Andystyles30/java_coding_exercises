4244eee56c32f44162c7e891842bb6cca1430868
Your Correct Name <andystyles30@gmail.com>
2020-08-28 18:07:44 +0100

    fix indentation

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -3,41 +3,41 @@ package com.techreturners.exercise001;
 import java.util.List;
 
 public class Exercise001 {
-    public String capitalizeWord(String word) {
-        if(word == null || word.isEmpty()) {
-            return word;
-        }
-    
-        return word.substring(0, 1).toUpperCase() + word.substring(1);
-    }
+	public String capitalizeWord(String word) {
+		if(word == null || word.isEmpty()) {
+			return word;
+		}
+	
+		return word.substring(0, 1).toUpperCase() + word.substring(1);
+	}
 
-    public String generateInitials(String firstName, String lastName) {
-        return firstName.substring(0, 1) + '.' + lastName.substring(0, 1);
-    }
+	public String generateInitials(String firstName, String lastName) {
+		return firstName.substring(0, 1) + '.' + lastName.substring(0, 1);
+	}
 
-    public double addVat(double originalPrice, double vatRate) {
-        double vat = ( originalPrice / 100 ) * vatRate;
-        double result = originalPrice + vat;
-        return Math.round(result * 100.0) / 100.0; // Using this to round to two decimal places - theres probably a better solution.
-    }
+	public double addVat(double originalPrice, double vatRate) {
+		double vat = ( originalPrice / 100 ) * vatRate;
+		double result = originalPrice + vat;
+		return Math.round(result * 100.0) / 100.0; // Using this to round to two decimal places - theres probably a better solution.
+	}
 
-    public String reverse(String sentence) {
-        String sentenceReversed = new StringBuilder(sentence).reverse().toString();
-        return sentenceReversed;
-    }
+	public String reverse(String sentence) {
+		String sentenceReversed = new StringBuilder(sentence).reverse().toString();
+		return sentenceReversed;
+	}
 
-    public int countLinuxUsers(List<User> users) {
-        // DID TRY THIS BUT LAMBDA EXPRESSIONS NOT SUPPORTED IN THIS VERSION
-        // long countLinuxUsers = users
-        //     .stream()
-        //     .filter(elem -> elem.getType() == "Linux")
-        //     .count();
-        int countLinuxUsers = 0;
-        for (User user : users) {
-          if (user.getType() == "Linux") {
-            countLinuxUsers++;
-          }
-        }
-        return countLinuxUsers;
-    }
+	public int countLinuxUsers(List<User> users) {
+		// DID TRY THIS BUT LAMBDA EXPRESSIONS NOT SUPPORTED IN THIS VERSION
+		// long countLinuxUsers = users
+		//     .stream()
+		//     .filter(elem -> elem.getType() == "Linux")
+		//     .count();
+		int countLinuxUsers = 0;
+		for (User user : users) {
+			if (user.getType() == "Linux") {
+			countLinuxUsers++;
+			}
+		}
+		return countLinuxUsers;
+	}
 }
